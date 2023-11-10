b3a55fecb7c2cfe18a6a0abb1c08a70a02355d6d
Senior-SergeantR <rabinotieno1@gmail.com>
2023-11-11 00:56:27 +0300

    a few lines of update with some js

--- script.js
+++ script.js
@@ -58,18 +58,22 @@ $('.gallery').magnificPopup({
 
 $('.testimonials-container').owlCarousel({
     loop:true,
+    autoplay:true,
+    autoplayTime:6000,
     margin:10,
     nav:true,
     navText:["<i class='fa-solid fa-arrow-left'></i>",
              "<i class='fa-solid fa-arrow-right'></i>"],
     responsive:{
         0:{
-            items:1
+            items:1,
+            nav:false
         },
         600:{
-            items:2
+            items:1,
+            nav:true
         },
-        1000:{
+        768:{
             items:2
         }
     }
