2b6d4e0bfc76db1320e297342b6bc1a784aa8d03
Senior-SergeantR <rabinotieno1@gmail.com>
2023-11-10 14:35:39 +0300

    finished the portfolio section added images heavy css and some js thats still not working

--- script.js
+++ script.js
@@ -30,3 +30,28 @@ function scrollFunction(){
     }
 
 }
+
+
+var $galleryContainer = $('.gallery').isotope({
+    itemSelector: '.item',
+    layoutMode: 'fitRows'
+})
+
+$('.button-group .button').on('click', function(){
+    $('.button-group .button').removeClass('active');
+    $(this).addClass('active');
+
+    var value = $(this).attr('data-filter');
+    $galleryContainer.isotope({
+        filter: value
+    })
+})
+
+
+$('.gallery').magnificPopup({
+    delegate: '.overlay a',
+    type: 'image',
+    gallery:{
+        enabled: true
+    }
+})
